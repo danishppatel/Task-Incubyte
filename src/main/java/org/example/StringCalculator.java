3df1734250a270d930f67240f9696721c9cf9b03
danishppatel <danishpatel5113@gmail.com>
2024-07-30 18:00:04 +0530

    Initial Task Structure Created...

---
+++ src/main/java/org/example/StringCalculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringCalculator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Developer Community!" );
+    }
+}
